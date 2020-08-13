e9b7481a2f8e9cc6b20a2c89b66a48e7d8d52ee2
julian141710 <jk981111@163.com>
2020-08-13 16:22:14 +0800

    更新了User.vue的数据交互

--- Doc/mysite/diamond_doc/urls.py
+++ Doc/mysite/diamond_doc/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('apis/user/login', views.UserMethod.login_user),  # 登录
     path('apis/user/logout', views.UserMethod.logout_user),  # 注销
     path('apis/user/register', views.UserMethod.register),  # 注册
+    path('apis/user/getstatus', views.UserMethod.get_status),  # 检查用户的登录状态
+    path('apis/user/getInfo', views.getInfo), #对应峥的User.vue 获取用户的昵称 密码 邮箱
+    path('apis/user/modify_nickname', views.UserMethod.modify_nickname),#对应峥的User.vue页面的修改昵称
+    path('path/user/logout', views.UserMethod.logout_user)#对应峥的退出登陆按钮
 ]
--- Doc/mysite/diamond_doc/views.py
+++ Doc/mysite/diamond_doc/views.py
@@ -11,7 +11,32 @@ from django.shortcuts import render
 def sayHello(request):
     return HttpResponse("hello world")
 
+def getInfo(request):
+    if request.method == "GET":
+        show_myInfo = request.GET.get("myInfo")
+
+        if show_myInfo is not None and show_myInfo == "true":
+            userInfo = UserInfo.objects.get(user = request.user)
+            return JsonResponse({
+                "status" : 0,
+                "nickname" : userInfo.user_nickname,
+                "password" : userInfo.user.password,
+                "email" : userInfo.user.email
+            })
+
 class UserMethod:
+    @staticmethod
+    def get_status(request):
+        if request.user.is_authenticated:
+            return JsonResponse({
+                "status": 0,
+                "username": str(request.user),
+                "email": str(request.user.email),
+            })
+        else:
+            return JsonResponse({
+                "status": 1
+            })
 
     @staticmethod
     def login_user(request):
@@ -109,3 +134,36 @@ class UserMethod:
                 "status": 1,
                 "message": "error method"
             })
+
+    @staticmethod
+    def modify_nickname(request):
+        if request.method == "POST":
+            data = json.loads(request.body)
+            new_nickname = data.get("nickname")
+
+            if len(new_nickname) == 0:
+                #昵称为空返回status和message
+                return JsonResponse({
+                    "status" : 0,
+                    "message" : "新昵称不可为空"
+                })
+            elif len(new_nickname) > 16 :
+                #昵称长度大于16返回status和message
+                return JsonResponse({
+                    "status": 0,
+                    "message": "新昵称过长"
+                })
+            else :
+                userInfo = UserInfo.objects.get(user = request.user)
+                userInfo.user_nickname = new_nickname
+                #修改成功返回status、message、以及修改后的新昵称new_nickname
+                return JsonResponse({
+                    "status" : 1,
+                    "message" : "昵称修改成功",
+                    "new_nickname" : new_nickname
+                })
+
+
+
+
+
